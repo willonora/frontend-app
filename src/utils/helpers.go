4a9785609c1714dff092338a791194700b712c61
willonora <262031431+willonora@users.noreply.github.com>
2026-04-03 18:26:13 +1000

    Add JSON helper functions for HTTP requests

---
+++ src/utils/helpers.go
@@ -0,0 +1,81 @@
+package helpers
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// ReadJSON reads JSON from request body and decodes into dst
+func ReadJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	maxBytes := 1 << 20 // 1MB
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
+	return nil
+}
+
+// WriteJSON writes data as JSON response
+func WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetFileExtension returns the lowercase extension of a filename
+func GetFileExtension(filename string) string {
+	ext := filepath.Ext(filename)
+	return strings.ToLower(ext)
+}
+
+// FileExists checks if a file exists and is not a directory
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// GenerateRandomString generates a random string of given length
+func GenerateRandomString(length int) (string, error) {
+	b := make([]byte, length)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(b), nil
+}
